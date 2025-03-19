56d2a39fa3a87f9e6554067b69b315715b6cfd66
octo <octo@thatoneocto.online>
2025-03-18 22:20:43 -0400

    fixed double zsh

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -29,26 +29,6 @@
       };
 
       home.sessionVariables = { EDITOR = "nvim"; VISUAL = "nvim"; MAKEFLAGS = "-j$(nproc)"; };
-      programs.zsh = {
-        enable = true;
-	enableCompletion = true;
-	autosuggestion.enable = true;
-	syntaxHighlighting.enable = true;
-	shellAliases = {
-          ls = "lsd -l";
-      	  ll = "lsd -la";
-      	  grep = "grep --color=auto";
-      	  c = "clear";
-      	  mkdir = "mkdir -p";
-      	  spt = "spotify_player";
-      	  update = "sudo nixos-rebuild switch";
-    	  }; 
-    	  initExtra = ''
-    	  any-nix-shell zsh --info-right | source /dev/stdin
-    	  export PS1=$'%{\e[255m%}%M%{\e[38;5;99m%}@%{\e[38;5;63m%}%n [%{\e[38;5;99m%}%~%{\e[38;5;63m%}] %{\e[36m%}%{\e[2m%}%{\e[0m%}(%?) > %{\e[255m%}'
-    	  fastfetch
-    	  '';
-	};
 
 	programs.kitty = {
 	  enable = true;
