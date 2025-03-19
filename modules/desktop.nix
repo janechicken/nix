6c7c749a568d578c90b165ceb738cb5a40be441b
octo <octo@thatoneocto.online>
2025-03-18 22:25:48 -0400

    fix dark theme

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -46,6 +46,8 @@
 	  theme = { name = "Adwaita"; };
 	  cursorTheme = { name = "Bibata-Modern-Classic"; package = pkgs.bibata-cursors; };
 	  iconTheme = { name = "Papirus"; package = pkgs.papirus-icon-theme; };
+	  gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    	  gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
 	};
 
 	qt = { enable = true; platformTheme.name = "gtk"; style.name = "adwaita-dark"; };
