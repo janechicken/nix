a4ce11278219b2eba20499e7c504d524d6126a9c
jane chicken <janechicken@purelymail.com>
2025-05-20 06:11:02 -0400

    update + input remapper

--- hosts/octo-pc/home.nix
+++ hosts/octo-pc/home.nix
@@ -45,6 +45,7 @@
       # (pkgs.writeShellScriptBin "my-hello" ''
       #   echo "Hello, ${config.home.username}!"
       # '')
+      input-remapper
     ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
