52e7e75fc6ad1a95c1dcc98006b8d3d513a4b204
jane chicken <janechicken@purelymail.com>
2025-08-01 11:44:05 -0400

    undid what was done

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,6 +1,4 @@
 { config, inputs, pkgs, lib, ... }: {
   services.flatpak.enable = true;
-  environment.profiles =
-    [ "$HOME/.local/share/flatpak/exports" "/var/lib/flatpak/exports" ];
   environment.systemPackages = with pkgs; [ ];
 }
