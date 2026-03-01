0c90377c67fd3feae9e0a7f0932a650645db7b6f
jane chicken <janechicken@purelymail.com>
2026-03-01 14:32:50 -0500

    no need for deepseek if using openrouter

--- secrets/secrets-root.nix
+++ secrets/secrets-root.nix
@@ -1,7 +1,6 @@
 { config, inputs, pkgs, lib, ...}:
 {
   environment.sessionVariables = {
-  DEEPSEEK_API_KEY = "$(cat /home/jane/.config/zed/deepseek_api_key)";
   OPENROUTER_API_KEY = "$(cat /home/jane/.config/zed/openrouter_api_key)";
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,10 +6,6 @@
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
     secrets = {
-      deepseek_api_key = {
-        sopsFile = ./secrets.yaml;
-        path = "${config.home.homeDirectory}/.config/zed/deepseek_api_key";
-      };
       openrouter_api_key = {
         sopsFile = ./secrets.yaml;
         path = "${config.home.homeDirectory}/.config/zed/openrouter_api_key";
