203d5c7a8b2cefa6dc206e47c325c2d22f242290
jane chicken <janechicken@purelymail.com>
2026-03-01 01:22:28 -0500

    zed changes

--- modules/zed.nix
+++ modules/zed.nix
@@ -33,7 +33,7 @@
       vscode-langservers-extracted
       nil
     ];
-    extensions = [ "lua" "nix" ];
+    extensions = [ "lua" "nix" "color highlight" ];
     userSettings = builtins.fromJSON (builtins.readFile ../dotfiles/zed/settings.json);
     userKeymaps = builtins.fromJSON (builtins.readFile ../dotfiles/zed/keymap.json);
     };
