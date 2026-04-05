2c695800082edc28934ce0cb96ae97c0e7eab382
jane chicken <janechicken@purelymail.com>
2026-04-05 01:09:40 -0400

    update

--- modules/core.nix
+++ modules/core.nix
@@ -16,7 +16,8 @@
     (lib.meta.setPrio 11 toybox)
     bat
     btop
-    wineWow64Packages.yabridge
+    # wineWow64Packages.yabridge
+    wineWow64Packages.staging
     winetricks
     home-manager
     zenity
@@ -24,6 +25,7 @@
     p7zip-rar
     qrrs
   ];
+  boot.loader.grub.configurationLimit = 3;
 
   nixpkgs.config = {
     allowBroken = true;
