b33e66031c1761b8daacad05173e3ca0b0cf536b
jane chicken <janechicken@purelymail.com>
2026-06-23 17:31:38 -0400

    joyshockmapper: fix remaining fake hash

--- pkgs/joyshockmapper/default.nix
+++ pkgs/joyshockmapper/default.nix
@@ -56,7 +56,7 @@ let
     owner = "Neargye";
     repo = "magic_enum";
     rev = "47e34ada93e0bf70dcea551636755cd66d893768";
-    hash = "sha256-tiou5sFNtbL110qK4MbjlY+x1yW8W6Irp6oMzuNM2I=";
+    hash = lib.fakeHash;
   };
 in
 stdenv.mkDerivation {
