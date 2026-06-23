99c02dc7b3015cf9ad6b1701b863a5575ffb0e0e
jane chicken <janechicken@purelymail.com>
2026-06-23 18:09:51 -0400

    joyshockmapper: add SDL_GAMEPADCONTROLLERCONFIG to module

--- modules/joyshockmapper.nix
+++ modules/joyshockmapper.nix
@@ -32,6 +32,10 @@ in
   };
 
   config = mkIf cfg.enable {
+    environment.sessionVariables = {
+      SDL_GAMEPADCONTROLLERCONFIG = "03000000c82d00001260000000000000,8BitDo Ultimate 2 Wireless,a:b0,b:b1,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,dpup:h0.1,leftshoulder:b6,leftstick:b13,lefttrigger:b8,leftx:a0,lefty:a2,paddle1:b11,paddle2:b12,paddle3:b13,paddle4:b14,rightshoulder:b7,rightstick:b14,righttrigger:b9,rightx:a3,righty:a5,x:b3,y:b4,
+05000000c82d00001260000000000000,8BitDo Ultimate 2 Wireless,a:b0,b:b1,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,dpup:h0.1,leftshoulder:b6,leftstick:b13,lefttrigger:b8,leftx:a0,lefty:a2,paddle1:b11,paddle2:b12,paddle3:b13,paddle4:b14,rightshoulder:b7,rightstick:b14,righttrigger:b9,rightx:a3,righty:a5,x:b3,y:b4,";
+    };
     environment.systemPackages = [ cfg.package ];
     services.udev.packages = [ cfg.package ];
 
